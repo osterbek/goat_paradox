2e39f1c2a24a184260756f791a17f3f5eb8dc662
Carsten <75850533+osterbek@users.noreply.github.com>
2024-02-10 16:30:45 +0100

    Update goat.py

--- goat.py
+++ goat.py
@@ -3,15 +3,13 @@ import random
 if __name__ == '__main__':
     counter_wins = [0, 0, 0]
     for game in range(1, 1001):
-        print("\nGame {}".format(game))
         winner_gate = random.randint(0, 2)
         selected_gate = random.randint(0, 2)
-        presented_empty_gate = (selected_gate + 1) % 3
-        if winner_gate == presented_empty_gate:
-            presented_empty_gate = (presented_empty_gate + 1) % 3
+        presented_empty_gate = random.choice(list({0, 1, 2} - {selected_gate, winner_gate}))
         third_gate = ({0, 1, 2} - {selected_gate, presented_empty_gate}).pop()
         strategy = ['Remain Gate', 'Fifty-Fifty', 'Change Gate']
         choice = [selected_gate, random.choice([selected_gate, third_gate]), third_gate]
+        print("\nGame {}".format(game))
         print(" Random winner gate: {}".format(winner_gate))
         print("    Random selected gate: {}".format(selected_gate))
         print("    Empty presented gate: {}".format(presented_empty_gate))
